ae9a1e2a22ef1c21c22fb8ea8bf21c8ee6c3dc4d
Caleb Rash <caleb.s.rash@gmail.com>
2017-10-01 13:38:43 -0500

    Add comments

--- djtest/urls.py
+++ djtest/urls.py
@@ -5,6 +5,9 @@ from djtest.views import CustomersListView, CustomerEditView
 
 urlpatterns = [
     url(r'^customers/$', CustomersListView.as_view(), name='customers_list'),
+
+    # Name url params by prefixing a pattern with `?P<...>`
     url(r'^customers/(?P<customer_id>\d+)/$', CustomerEditView.as_view(), name='customers_edit'),
+
     url(r'^admin/', admin.site.urls),
 ]
--- djtest/views.py
+++ djtest/views.py
@@ -13,6 +13,10 @@ class CustomersListView(View):
 class CustomerEditView(View):
 
     def get(self, request, customer_id):
+        """
+        Handle GET requests in the `get` handler. `customer_id` comes from the url regexp in
+        urls.py.
+        """
         customer = Customer.objects.get(id=customer_id)
         form = CustomerEditForm(instance=customer)
 
@@ -22,6 +26,11 @@ class CustomerEditView(View):
         })
 
     def post(self, request, customer_id):
+        """
+        Handle POST requests in the `post` handler. In this function, we pass the POST object to
+        the form to validate and save our udpated model. If the form is invalid, validations
+        errors will appear in the form.
+        """
         customer = Customer.objects.get(id=customer_id)
         form = CustomerEditForm(request.POST, instance=customer)
 
